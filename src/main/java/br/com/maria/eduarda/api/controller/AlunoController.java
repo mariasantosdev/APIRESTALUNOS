63642a64257708339d272106add96cbfdcc4e089
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2021-03-19 14:41:58 -0300

    Adição do método remover

--- src/main/java/br/com/maria/eduarda/api/controller/AlunoController.java
+++ src/main/java/br/com/maria/eduarda/api/controller/AlunoController.java
@@ -3,11 +3,13 @@ package br.com.maria.eduarda.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import br.com.maria.eduarda.api.model.Aluno;
 import br.com.maria.eduarda.api.repository.AlunoRepository;
@@ -67,6 +68,19 @@ public class AlunoController {
 		
 		return ResponseEntity.ok(aluno);
 	}
+	
+	@DeleteMapping("/{alunoId}")
+	public ResponseEntity<Void> remover(@PathVariable Long alunoId) {
+		if (!alunoRepository.existsById(alunoId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		alunoRepository.deleteById(alunoId);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+
 	
 	
 	
